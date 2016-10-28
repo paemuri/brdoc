f575622e17c066df1d3b527fa23422b18b723e8a
Rafael Escobar <rafael@nhanderu.com>
2016-10-28 18:12:02 -0200

    Create CNPJ tests.

--- cpfcnpj_test.go
+++ cpfcnpj_test.go
@@ -37,6 +37,39 @@ func TestIsCPF(t *T) {
     })
 }
 
+func TestIsCNPJ(t *T) {
+    t.Run("Invalid CNPJ format", func(t *T) {
+        v := IsCNPJ("3467875434578764345789654")
+        assert(t, false, v)
+        
+        v = IsCNPJ("123")
+        assert(t, false, v)
+        
+        v = IsCNPJ("#$%¨&*(ABCDEF")
+        assert(t, false, v)
+    })
+    t.Run("Invalid digits in CNPJ", func(t *T) {
+        v := IsCNPJ("00.000.000/0000-11")
+        assert(t, false, v)
+        
+        v = IsCNPJ("11.111.111/1111-00")
+        assert(t, false, v)
+        
+        v = IsCNPJ("28.637.456/1000-95")
+        assert(t, false, v)
+    })
+    t.Run("Valid CNPJ", func(t *T) {
+        v := IsCNPJ("00.000.000/0000-00")
+        assert(t, false, v)
+        
+        v = IsCNPJ("11.111.111/1111-80")
+        assert(t, false, v)
+        
+        v = IsCNPJ("28.637.456/1000-59")
+        assert(t, false, v)
+    })
+}
+
 func assert(t *T, expected bool, value bool) {
 	if value == expected {
 		t.Logf("Result should be \"%v\": ja!", expected)
