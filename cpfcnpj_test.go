b85573e5a9cf16de987a4059f8a41f714ae352af
Rafael Escobar <rafael@nhanderu.com>
2016-11-22 13:45:45 -0200

    Separate test utils in another file.

--- cpfcnpj_test.go
+++ cpfcnpj_test.go
@@ -102,11 +102,3 @@ func TestIsCNPJ(t *T) {
 		}
 	})
 }
-
-func assert(t *T, value string, result, expected bool) {
-	if result == expected {
-		t.Logf("The result of %s should be \"%v\": ja!", value, expected)
-	} else {
-		t.Errorf("The result of %s should be \"%v\": nein!", value, expected)
-	}
-}
---
+++ testutils.go
@@ -0,0 +1,11 @@
+package brdoc
+
+import . "testing"
+
+func assert(t *T, value string, result, expected bool) {
+	if result == expected {
+		t.Logf("The result of %s should be \"%v\": ja!", value, expected)
+	} else {
+		t.Errorf("The result of %s should be \"%v\": nein!", value, expected)
+	}
+}
