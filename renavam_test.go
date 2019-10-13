a58ca358769e83e79ef4e8f9ec488a48325adc7c
Leonardo Gregianin <leogregianin@gmail.com>
2019-10-13 11:05:25 -0400

    closes #8 Reescrevi a função e adicionei mais testes

--- renavam_test.go
+++ renavam_test.go
@@ -11,11 +11,19 @@ func TestIsRenavam(t *testing.T) {
 	}{
 		// Valid
 		{"81285820857", true},
-		{"17058862280", true},
+		{"03135375023", true},
+		{"10016211275", true},
+		{"38872054176", true},
+		{"40999838201", true},
+		{"31789431483", true},
 
 		// Invalid
-		{"17058862281", false},
+		{"38872054170", false},
+		{"40999838209", false},
+		{"31789431480", false},
 		{"38919643060", false},
+		{"123", false},
+		{"abcdefghijk", false},
 	} {
 		t.Logf("#%d Renavam validation of %s should return %v: ", i, v.v, v.r)
 		if IsRenavam(v.v) != v.r {
