dde01ee0333d107a34502395267f9278ca4f0728
Robas <rodrigo.olopes@mercadolivre.com>
2023-03-24 13:09:13 -0300

    Add RG validation for SP and RJ

--- utils_test.go
+++ utils_test.go
@@ -15,6 +15,26 @@ actual:   %v
 	}
 }
 
+func assertIntEqual(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Fatalf(`
+Not equal!
+expected: %v
+actual:   %v
+`, expected, actual)
+	}
+}
+
+func assertStringEqual(t *testing.T, expected, actual string) {
+	if expected != actual {
+		t.Fatalf(`
+Not equal!
+expected: %v
+actual:   %v
+`, expected, actual)
+	}
+}
+
 func testName(i int, name string) string {
 	return fmt.Sprintf("%d_%s", i, name)
 }
