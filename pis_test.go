6b52314429d4f54ad9053896ebc5a4560426e6a5
Rafael Escobar <rafael@paemuri.com>
2023-09-19 21:57:49 +0200

    Refactor some tests

--- pis_test.go
+++ pis_test.go
@@ -10,10 +10,14 @@ func TestIsPIS(t *testing.T) {
 		expected bool
 		v        string
 	}{
+		{"InvalidData_ShouldReturnFalse", false, "3467875434578764345789654"},
 		{"InvalidData_ShouldReturnFalse", false, ""},
-		{"InvalidData_ShouldReturnFalse", false, "11111111111"},
-		{"InvalidData_ShouldReturnFalse", false, "103951990150"},
-		{"InvalidData_ShouldReturnFalse", false, "103.951.9901-0"},
+		{"InvalidData_ShouldReturnFalse", false, "AAAAAAAAAAA"},
+		{"InvalidPattern_ShouldReturnFalse", false, "103.951.990-15"},
+		{"InvalidPattern_ShouldReturnFalse", false, "103 951 990 15"},
+		{"InvalidPattern_ShouldReturnFalse", false, "103951-99015"},
+		{"InvalidDigits_ShouldReturnFalse", false, "103.95199.01-6"},
+		{"InvalidDigits_ShouldReturnFalse", false, "120.1641.414-9"},
 		{"Valid_ShouldReturnTrue", true, "103.951.9901-5"},
 		{"Valid_ShouldReturnTrue", true, "103.95199.01-5"},
 		{"Valid_ShouldReturnTrue", true, "120.6372.482-4"},
